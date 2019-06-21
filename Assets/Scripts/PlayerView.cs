45c1f74e9bb4adacf4f967077878f27b9c6b7ab6
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-21 23:27:32 +0900

    PlayerViewクラスを更新

--- Assets/Scripts/PlayerView.cs
+++ Assets/Scripts/PlayerView.cs
@@ -1,19 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class PlayerView : MonoBehaviour, IPlayerView
 {
     private Transform tr;
-    public Transform Tr => tr ? tr : tr = this.GetComponent<Transform>();
+    public Transform Tr => tr;
+
+    //private void Awake()
+    //{
+    //    tr = this.GetComponent<Transform>();
+    //}
+
+    [Inject]
+    private void Construct()
+    {
+        tr = this.GetComponent<Transform>();
+    }
 
     //Transformの更新
-    public void UpdateTransform(Transform tr){
-        this.tr = tr;
+    public void UpdatePosition(Vector3 pos){
+        this.tr.position = pos;
     }
 }
 
 public interface IPlayerView {
     Transform Tr{get;}
-    void UpdateTransform(Transform tr);
+    void UpdatePosition(Vector3 pos);
 }
