8c93e7f0d733864bb600139d26b4cb99beda92af
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-21 14:36:36 +0900

    PlayerView,IPlayerViewを作成

---
+++ Assets/Scripts/PlayerView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour, IPlayerView
+{
+    private Transform tr;
+    public Transform Tr => tr ? tr : tr = this.GetComponent<Transform>();
+
+    //Transformの更新
+    public void UpdateTransform(Transform tr){
+        this.tr = tr;
+    }
+}
+
+public interface IPlayerView {
+    Transform Tr{get;}
+    void UpdateTransform(Transform tr);
+}
