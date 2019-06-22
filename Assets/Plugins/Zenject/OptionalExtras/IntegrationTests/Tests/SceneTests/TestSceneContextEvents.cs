affb81b5353a593efe265cf45c8f838911b29944
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-22 12:05:26 +0900

    no message

---
+++ Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Tests/SceneTests/TestSceneContextEvents.cs
@@ -0,0 +1,20 @@
+#if UNITY_EDITOR
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Zenject.Tests
+{
+    public class TestSceneContextEvents : SceneTestFixture
+    {
+        [UnityTest]
+        public IEnumerator TestScene()
+        {
+            yield return LoadScene("TestSceneContextEvents");
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
+
+#endif
