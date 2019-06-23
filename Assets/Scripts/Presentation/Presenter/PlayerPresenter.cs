1fd1376301e633b13b258e9af6eb114a1d6a2257
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-23 20:40:57 +0900

    PlayerPresenterクラスを作成

---
+++ Assets/Scripts/Presentation/Presenter/PlayerPresenter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Zenject;
+using Domain.UseCase;
+
+namespace Presentation.Presenter
+{
+    public class PlayerPresenter : IPlayerPresenter{
+
+        private IPlayerView playerView;
+
+        public Transform Tr => playerView.Tr;
+
+        [Inject] IFactory<IPlayerView> playerViewFactory;
+
+        public IPlayerView CreatePlayer(){
+            playerView =  playerViewFactory.Create();
+            return playerView;
+        }
+
+        public void UpdatePlayerPosition(Vector3 pos){
+            playerView.UpdatePosition(pos);
+        }
+    }
+}
