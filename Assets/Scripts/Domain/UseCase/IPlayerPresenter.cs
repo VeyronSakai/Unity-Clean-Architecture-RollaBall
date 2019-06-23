dc15a961dcc62d202773d3e0215d07416446abbc
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-23 20:40:41 +0900

    IPlayerPresenterインターフェースを作成

---
+++ Assets/Scripts/Domain/UseCase/IPlayerPresenter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Presentation.Presenter;
+
+namespace Domain.UseCase
+{
+    public interface IPlayerPresenter
+    {
+        Transform Tr { get; }
+        IPlayerView CreatePlayer();
+        void UpdatePlayerPosition(Vector3 pos);
+    }
+}
