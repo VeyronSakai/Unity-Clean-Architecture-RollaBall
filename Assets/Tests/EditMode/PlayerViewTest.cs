c01f96019dd0870cabe06282b41dcb43367dbd00
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-23 21:10:55 +0900

    PlayerViewの単体テストを作成

---
+++ Assets/Tests/EditMode/PlayerViewTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Zenject;
+using Presentation.View;
+using Presentation.Presenter;
+
+namespace Tests
+{
+    public class PlayerViewTest : ZenjectUnitTestFixture
+    {
+        GameObject playerPrefab;
+        IPlayerView playerView;
+        IPlayerView playerViewInstance;
+        IFactory<IPlayerView> playerPrefabFactory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            playerPrefab = new GameObject();
+            playerPrefab.AddComponent<PlayerView>();
+
+            Container
+                .BindIFactory<IPlayerView>()
+                .To<PlayerView>()
+                .FromComponentInNewPrefab(playerPrefab);
+
+            Container
+                .Bind<IPlayerView>()
+                .To<PlayerView>()
+                .AsTransient();
+
+            playerPrefabFactory = Container.Resolve<IFactory<IPlayerView>>();
+        }
+
+        [Test]
+        public void InitializeTest()
+        {
+
+            Assert.IsNotNull(playerPrefabFactory);
+            Assert.IsNull(playerViewInstance);
+            playerViewInstance = playerPrefabFactory.Create();
+            Assert.IsNotNull(playerViewInstance);
+
+            Assert.AreEqual(new Vector3(0, 1, 0), playerViewInstance.Tr.position);
+        }
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void UpdatePositionTest()
+        {
+            // Use the Assert class to test conditions
+            playerViewInstance = playerPrefabFactory.Create();
+            playerViewInstance.UpdatePosition(new Vector3(1, 1, 0));
+            Assert.AreEqual(1, playerViewInstance.Tr.position.x);
+            playerViewInstance.UpdatePosition(new Vector3(2, 1, 2));
+            Assert.AreEqual(new Vector3(2, 1, 2), playerViewInstance.Tr.position);
+        }
+    }
+}
