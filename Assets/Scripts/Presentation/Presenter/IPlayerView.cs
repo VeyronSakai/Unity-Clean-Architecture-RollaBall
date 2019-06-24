52aeebf06fd2e1c5ab45779c5fd6a3897ffb6038
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-24 23:09:17 +0900

    PlayerのTransformをReactivePropertyで管理する

--- Assets/Scripts/Presentation/Presenter/IPlayerView.cs
+++ Assets/Scripts/Presentation/Presenter/IPlayerView.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UniRx;
 
 namespace Presentation.Presenter
 {
     public interface IPlayerView {
         Transform Tr{get;}
+        IReadOnlyReactiveProperty<Transform> TransformProperty { get; }
         void UpdatePosition(Vector3 pos);
         void DestroyPlayer();
     }
--- Assets/Scripts/Presentation/View/PlayerView.cs
+++ Assets/Scripts/Presentation/View/PlayerView.cs
@@ -1,24 +1,28 @@
 ﻿using UnityEngine;
 using Zenject;
 using Presentation.Presenter;
+using UniRx;
 
 namespace Presentation.View
 {   public class PlayerView : MonoBehaviour, IPlayerView
     {
-        private Transform tr;
-        public Transform Tr => tr;
+        // PlayerのTransformをReactivePropertyで管理
+        private ReactiveProperty<Transform> _trProperty;
+        public IReadOnlyReactiveProperty<Transform> TransformProperty => _trProperty;
+        public Transform Tr => _trProperty.Value;
 
         // PlayerViewインスタンス生成時に自動的に実行される
         [Inject]
         private void Initialize()
         {
-            tr = this.GetComponent<Transform>();
+            _trProperty = new ReactiveProperty<Transform>();
+            _trProperty.Value = this.GetComponent<Transform>();
             UpdatePosition(new Vector3(0,1,0));
         }
 
         //Transformの更新
         public void UpdatePosition(Vector3 pos){
-            this.tr.position = pos;
+            this._trProperty.Value.position = pos;
         }
 
         public void DestroyPlayer()
