cf11d06adcac33cc6a930df0454cd5541cd57ae5
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-25 10:45:36 +0900

    コメントを追記

---
+++ Assets/Scripts/Domain/UseCase/DestroyPlayerUseCase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using UniRx;
+
+namespace Domain.UseCase
+{
+    public class DestroyPlayerUseCase : IDestroyPlayerUseCase, IInitializable
+    {
+        [Inject] IPlayerPresenter playerPresenter;
+
+        void IInitializable.Initialize()
+        {
+            // Playerが場外に落ちた場合、Playerを消去して、新たにPlayerを生成する
+            playerPresenter
+                .TransformProperty
+                .Where(x => x.position.y < -1)
+                .Subscribe(_ => ReInitializePlayer());
+        }
+
+        public void ReInitializePlayer()
+        {
+            playerPresenter.DestroyPlayer();
+            playerPresenter.CreatePlayer();
+        }
+    }
+
+    public interface IDestroyPlayerUseCase
+    {
+
+    }
+}
+
+
