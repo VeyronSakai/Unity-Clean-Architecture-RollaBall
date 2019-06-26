9da5d961e20309c6a69ed649f7572c6071f768f7
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-26 13:27:48 +0900

    InputPresenterクラスを作成

---
+++ Assets/Scripts/Presentation/Presenter/InputPresenter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Zenject;
+using UniRx;
+using System;
+using Domain.UseCase;
+
+namespace Presentation.Presenter
+{
+    public class InputPresenter : IInputPresenter
+    {
+        [Inject] private IInputView inputView;
+
+        public IObservable<Unit> OnUpKeyDownAsObservable()
+        {
+            return inputView.OnUpKeyDownAsObservable();
+        }
+
+        public IObservable<Unit> OnRightKeyDownAsObservable()
+        {
+            return inputView.OnRightKeyDownAsObservable();
+        }
+
+        public IObservable<Unit> OnDownKeyDownAsObservable()
+        {
+            return inputView.OnDownKeyDownAsObservable();
+        }
+
+        public IObservable<Unit> OnLeftKeyDownAsObservable()
+        {
+            return inputView.OnLeftKeyDownAsObservable();
+        }
+    }
+
+}
+
