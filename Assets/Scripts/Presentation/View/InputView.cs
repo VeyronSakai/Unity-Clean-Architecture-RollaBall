1769ee5f63ccf77eb0b922e529aca313e71827fa
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-26 13:06:00 +0900

    InputViewクラスを作成

---
+++ Assets/Scripts/Presentation/View/InputView.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
+using Presentation.Presenter;
+
+namespace Presentation.View
+{
+    public class InputView : IInputView
+    {
+        public IObservable<Unit> OnUpKeyDownAsObservable()
+        {
+            return Observable.EveryUpdate().Where(_ => Input.GetKeyDown(KeyCode.UpArrow)).AsUnitObservable();
+        }
+
+        public IObservable<Unit> OnRightKeyDownAsObservable()
+        {
+            return Observable.EveryUpdate().Where(_ => Input.GetKeyDown(KeyCode.RightArrow)).AsUnitObservable();
+        }
+
+        public IObservable<Unit> OnDownKeyDownAsObservable()
+        {
+            return Observable.EveryUpdate().Where(_ => Input.GetKeyDown(KeyCode.DownArrow)).AsUnitObservable();
+        }
+
+        public IObservable<Unit> OnLeftKeyDownAsObservable()
+        {
+            return Observable.EveryUpdate().Where(_ => Input.GetKeyDown(KeyCode.LeftArrow)).AsUnitObservable();
+        }
+    }
+}
+
+
