60caad49b1332709f7429007869a3f2166402f65
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-26 13:28:02 +0900

    IInputPresenterインターフェースを作成

---
+++ Assets/Scripts/Domain/UseCase/IInputPresenter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UniRx;
+using System;
+
+namespace Domain.UseCase
+{
+    public interface IInputPresenter
+    {
+        IObservable<Unit> OnUpKeyDownAsObservable();
+        IObservable<Unit> OnRightKeyDownAsObservable();
+        IObservable<Unit> OnDownKeyDownAsObservable();
+        IObservable<Unit> OnLeftKeyDownAsObservable();
+    }
+}
+
