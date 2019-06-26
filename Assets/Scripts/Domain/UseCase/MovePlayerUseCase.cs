c3f2d5ef52bee86e2c29f4d30185880c2c695ca7
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-26 14:15:45 +0900

    MovePlayerUseCaseを作成

---
+++ Assets/Scripts/Domain/UseCase/MovePlayerUseCase.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using Zenject;
+using UniRx;
+using UniRx.Triggers;
+using Presentation.Presenter;
+
+namespace Domain.UseCase
+{
+    public class MovePlayerUseCase : IMovePlayerUseCase, IInitializable
+    {
+        [Inject] private IPlayerPresenter playerPresenter;
+
+        [Inject] private IInputPresenter inputPresenter;
+
+        private Vector3 moveDirection;
+
+        void IInitializable.Initialize()
+        {
+            Observable
+                .EveryUpdate()
+                .Subscribe(_ => playerPresenter.Move(moveDirection));
+
+            playerPresenter
+                .Move(moveDirection);
+
+            inputPresenter
+                .OnUpKeyDownAsObservable()
+                .Subscribe(_ => moveDirection.z = 1);
+
+            inputPresenter
+                .OnRightKeyDownAsObservable()
+                .Subscribe(_ => moveDirection.x = 1);
+
+            inputPresenter
+                .OnDownKeyDownAsObservable()
+                .Subscribe(_ => moveDirection.z = -1);
+
+            inputPresenter
+                .OnLeftKeyDownAsObservable()
+                .Subscribe(_ => moveDirection.x = -1);
+
+            inputPresenter
+                .OnUpKeyUpAsObservable()
+                .Subscribe(_ => moveDirection.z = 0);
+
+            inputPresenter
+                .OnRightKeyUpAsObservable()
+                .Subscribe(_ => moveDirection.x = 0);
+
+            inputPresenter
+                .OnDownKeyUpAsObservable()
+                .Subscribe(_ => moveDirection.z = 0);
+
+            inputPresenter
+                .OnLeftKeyUpAsObservable()
+                .Subscribe(_ => moveDirection.x = 0);
+        }
+    }
+
+    public interface IMovePlayerUseCase
+    {
+
+    }
+}
+
+
