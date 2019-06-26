30e24048b1a6124f33190b6e7b3b72281653cce0
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-26 22:31:39 +0900

    GetScoreUseCaseを作成

---
+++ Assets/Scripts/Domain/UseCase/GetScoreUseCase.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Zenject;
+using Domain.Entity;
+using UniRx;
+
+namespace Domain.UseCase
+{
+    public class GetScoreUseCase : IGetScoreUseCase, IInitializable
+    {
+        [Inject] IBlockPresenter blockPresenter;
+        [Inject] IScoreEntity scoreEntity;
+
+        void IInitializable.Initialize()
+        {
+
+        }
+    }
+
+    public interface IGetScoreUseCase
+    {
+
+    }
+}
