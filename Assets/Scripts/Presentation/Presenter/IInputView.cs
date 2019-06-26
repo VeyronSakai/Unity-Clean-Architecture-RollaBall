ee652c4918c2fd5b9c349d558ec37196fe985ddf
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-26 13:06:22 +0900

    IInputViewインターフェースを作成

---
+++ Assets/Scripts/Presentation/Presenter/IInputView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+
+namespace Presentation.Presenter
+{
+    public interface IInputView
+    {
+        IObservable<Unit> OnUpKeyDownAsObservable();
+        IObservable<Unit> OnRightKeyDownAsObservable();
+        IObservable<Unit> OnDownKeyDownAsObservable();
+        IObservable<Unit> OnLeftKeyDownAsObservable();
+    }
+}
+
