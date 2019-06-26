06c6cba1eea12ffa7c348719d26f104996a3b37d
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-26 17:39:52 +0900

    IBlockViewインターフェースを作成

---
+++ Assets/Scripts/Presentation/Presenter/Interfaces/IBlockView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
+
+namespace Presentation.Presenter
+{
+    public interface IBlockView
+    {
+        IObservable<Unit> OnTriggerEnterPlayerAsObservable();
+    }
+
+}
+
