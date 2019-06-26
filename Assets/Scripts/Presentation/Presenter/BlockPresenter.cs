dfa08334396863ebde517a25a6826dc7d0aaf8cf
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-26 17:39:28 +0900

    BlockPresenterクラスを作成

---
+++ Assets/Scripts/Presentation/Presenter/BlockPresenter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using Domain.UseCase;
+using Zenject;
+using UniRx;
+using UniRx.Triggers;
+
+namespace Presentation.Presenter
+{
+    public class BlockPresenter : IBlockPresenter
+    {
+        [Inject] private IBlockView blockView;
+
+        public IObservable<Unit> OnTriggerEnterPlayerAsObservable()
+        {
+            return blockView.OnTriggerEnterPlayerAsObservable();
+        }
+    }
+}
