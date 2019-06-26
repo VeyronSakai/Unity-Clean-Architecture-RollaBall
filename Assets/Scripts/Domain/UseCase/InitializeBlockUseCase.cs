94658f4a51e9be2a40a3bd951c3512bab209798a
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-26 22:31:57 +0900

    InitializeBlockUseCaseを作成

---
+++ Assets/Scripts/Domain/UseCase/InitializeBlockUseCase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Domain.UseCase
+{
+    public class InitializeBlockUseCase : IInitializable, IInitializeBlockUseCase
+    {
+        [Inject] private IBlockPresenter blockPresenter;
+
+        void IInitializable.Initialize()
+        {
+            blockPresenter.InstantiateBlock(new Vector3(0, 1, 3));
+            blockPresenter.InstantiateBlock(new Vector3(0, 1, -3));
+            blockPresenter.InstantiateBlock(new Vector3(3, 1, 0));
+            blockPresenter.InstantiateBlock(new Vector3(-3, 1, 0));
+        }
+    }
+
+    public interface IInitializeBlockUseCase
+    {
+
+    }
+
+
+}
