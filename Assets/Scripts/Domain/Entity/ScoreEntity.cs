7fcbe913a42178f5273e8c4ced4886e28578ea3c
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-26 20:27:23 +0900

    ScoreEntityを作成

---
+++ Assets/Scripts/Domain/Entity/ScoreEntity.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UniRx;
+
+namespace Domain.Entity
+{
+    public class ScoreEntity
+    {
+        private ReactiveProperty<int> _scoreProperty;
+        public IReadOnlyReactiveProperty<int> ScoreProperty => _scoreProperty;
+
+        public ScoreEntity()
+        {
+            _scoreProperty = new ReactiveProperty<int>(0);
+        }
+
+        public void Increment()
+        {
+            _scoreProperty.Value++;
+        }
+    }
+
+    public interface IScoreEntity
+    {
+        IReadOnlyReactiveProperty<int> ScoreProperty { get; }
+        void Increment();
+    }
+}
+
+
