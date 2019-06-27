a55cd1f3562a763cdd0f0b0e3a6097838bae87e9
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-27 14:48:53 +0900

    IScorePresenterを作成

---
+++ Assets/Scripts/Domain/UseCase/Interfaces/IScorePresenter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Domain.UseCase
+{
+    public interface IScorePresenter
+    {
+        void UpdateScore(int num);
+    }
+
+}
