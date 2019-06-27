cbcf53b7e0aacb4f396898d1631a555fe2fbdb57
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-27 15:42:41 +0900

    UpdateScoreUseCaseを作成

---
+++ Assets/Scripts/Domain/UseCase/UpdateScoreUseCase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+using Domain.Entity;
+using UniRx;
+
+namespace Domain.UseCase
+{
+    public class UpdateScoreUseCase : IUpdateScoreUseCase, IInitializable
+    {
+        [Inject] private IScorePresenter scorePresenter;
+        [Inject] private IScoreEntity scoreEntity;
+
+        void IInitializable.Initialize()
+        {
+            scoreEntity
+                .ScoreProperty
+                .Subscribe(score => scorePresenter.UpdateScore(score));
+        }
+    }
+
+    public interface IUpdateScoreUseCase
+    {
+
+    }
+
+}
+
