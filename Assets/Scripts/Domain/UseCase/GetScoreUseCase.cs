e8ddad186f93c71eceb6592a41baf8af09536b97
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-27 12:02:26 +0900

    GetScoreUseCaseにscoreEntityを操作する処理を追記

--- Assets/Scripts/Domain/UseCase/GetScoreUseCase.cs
+++ Assets/Scripts/Domain/UseCase/GetScoreUseCase.cs
@@ -13,15 +13,38 @@ namespace Domain.UseCase
         void IInitializable.Initialize()
         {
             // for文を使うと何故かエラーになってしまう
-            blockPresenter.OnTriggerEnterPlayerAsObservable(0).Subscribe(_ => blockPresenter.BlockList[0].DestroyBlock());
-            blockPresenter.OnTriggerEnterPlayerAsObservable(1).Subscribe(_ => blockPresenter.BlockList[1].DestroyBlock());
-            blockPresenter.OnTriggerEnterPlayerAsObservable(2).Subscribe(_ => blockPresenter.BlockList[2].DestroyBlock());
-            blockPresenter.OnTriggerEnterPlayerAsObservable(3).Subscribe(_ => blockPresenter.BlockList[3].DestroyBlock());
-            blockPresenter.OnTriggerEnterPlayerAsObservable(4).Subscribe(_ => blockPresenter.BlockList[4].DestroyBlock());
-            blockPresenter.OnTriggerEnterPlayerAsObservable(5).Subscribe(_ => blockPresenter.BlockList[5].DestroyBlock());
-            blockPresenter.OnTriggerEnterPlayerAsObservable(6).Subscribe(_ => blockPresenter.BlockList[6].DestroyBlock());
-            blockPresenter.OnTriggerEnterPlayerAsObservable(7).Subscribe(_ => blockPresenter.BlockList[7].DestroyBlock());
-
+            blockPresenter.OnTriggerEnterPlayerAsObservable(0).Subscribe(_ => {
+                blockPresenter.BlockList[0].DestroyBlock();
+                scoreEntity.Increment();
+                });
+            blockPresenter.OnTriggerEnterPlayerAsObservable(1).Subscribe(_ => {
+                blockPresenter.BlockList[1].DestroyBlock();
+                scoreEntity.Increment();
+                });
+            blockPresenter.OnTriggerEnterPlayerAsObservable(2).Subscribe(_ => {
+                blockPresenter.BlockList[2].DestroyBlock();
+                scoreEntity.Increment();
+                });
+            blockPresenter.OnTriggerEnterPlayerAsObservable(3).Subscribe(_ => {
+                blockPresenter.BlockList[3].DestroyBlock();
+                scoreEntity.Increment();
+                });
+            blockPresenter.OnTriggerEnterPlayerAsObservable(4).Subscribe(_ => {
+                blockPresenter.BlockList[4].DestroyBlock();
+                scoreEntity.Increment();
+                });
+            blockPresenter.OnTriggerEnterPlayerAsObservable(5).Subscribe(_ => {
+                blockPresenter.BlockList[5].DestroyBlock();
+                scoreEntity.Increment();
+                });
+            blockPresenter.OnTriggerEnterPlayerAsObservable(6).Subscribe(_ => {
+                blockPresenter.BlockList[6].DestroyBlock();
+                scoreEntity.Increment();
+                });
+            blockPresenter.OnTriggerEnterPlayerAsObservable(7).Subscribe(_ => {
+                blockPresenter.BlockList[7].DestroyBlock();
+                scoreEntity.Increment();
+                });
         }
     }
 
