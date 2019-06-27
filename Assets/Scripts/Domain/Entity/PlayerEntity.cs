6df113e758a79d5754aca980d122ccf9aeeb2f12
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-27 15:50:12 +0900

    不要な処理を削除

--- Assets/Scripts/Domain/Entity/PlayerEntity.cs
+++ Assets/Scripts/Domain/Entity/PlayerEntity.cs
@@ -6,14 +6,7 @@ namespace Domain.Entity
 {
     public class PlayerEntity : IPlayerEntity
     {
-        private ReactiveProperty<Vector3> _playerPositionProperty;
-        public IReadOnlyReactiveProperty<Vector3> PlayerPositionProperty => _playerPositionProperty;
-        public Vector3 PlayerPostion => _playerPositionProperty.Value;
 
-        public PlayerEntity()
-        {
-            _playerPositionProperty = new ReactiveProperty<Vector3>(new Vector3(0, 1, 0));
-        }
     }
 }
 
