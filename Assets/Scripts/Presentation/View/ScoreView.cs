dac11fc1853f2cda6bd93c2a5079afb37211088e
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-27 14:42:09 +0900

    ScoreViewを作成

---
+++ Assets/Scripts/Presentation/View/ScoreView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Presentation.Presenter;
+using TMPro;
+
+namespace Presentation.View
+{
+    public class ScoreView : MonoBehaviour, IScoreView
+    {
+        private TextMeshProUGUI textMeshPro;
+
+        private void Start()
+        {
+            textMeshPro = this.GetComponent<TextMeshProUGUI>();
+        }
+
+        public void UpdateScore(int num)
+        {
+            textMeshPro.text = num.ToString();
+        }
+    }
+}
