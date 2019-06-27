c1c0ea8b6e0835355e106b78e8190b8a8c6fc3b4
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-27 11:43:35 +0900

    GetScoreUseCaseを変更

--- Assets/Scripts/Domain/UseCase/GetScoreUseCase.cs
+++ Assets/Scripts/Domain/UseCase/GetScoreUseCase.cs
@@ -12,6 +12,15 @@ namespace Domain.UseCase
 
         void IInitializable.Initialize()
         {
+            // for文を使うと何故かエラーになってしまう
+            blockPresenter.OnTriggerEnterPlayerAsObservable(0).Subscribe(_ => blockPresenter.BlockList[0].DestroyBlock());
+            blockPresenter.OnTriggerEnterPlayerAsObservable(1).Subscribe(_ => blockPresenter.BlockList[1].DestroyBlock());
+            blockPresenter.OnTriggerEnterPlayerAsObservable(2).Subscribe(_ => blockPresenter.BlockList[2].DestroyBlock());
+            blockPresenter.OnTriggerEnterPlayerAsObservable(3).Subscribe(_ => blockPresenter.BlockList[3].DestroyBlock());
+            blockPresenter.OnTriggerEnterPlayerAsObservable(4).Subscribe(_ => blockPresenter.BlockList[4].DestroyBlock());
+            blockPresenter.OnTriggerEnterPlayerAsObservable(5).Subscribe(_ => blockPresenter.BlockList[5].DestroyBlock());
+            blockPresenter.OnTriggerEnterPlayerAsObservable(6).Subscribe(_ => blockPresenter.BlockList[6].DestroyBlock());
+            blockPresenter.OnTriggerEnterPlayerAsObservable(7).Subscribe(_ => blockPresenter.BlockList[7].DestroyBlock());
 
         }
     }
