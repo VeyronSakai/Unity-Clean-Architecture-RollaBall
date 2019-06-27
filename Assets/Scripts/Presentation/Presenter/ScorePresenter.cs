b89a56c8e38aaf4cf795ae31b4c80d37ab706387
Yuki Sakai <yuukisakai123@gmail.com>
2019-06-27 14:45:44 +0900

    ScorePresenterを作成

---
+++ Assets/Scripts/Presentation/Presenter/ScorePresenter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+namespace Presentation.Presenter
+{
+    public class ScorePresenter
+    {
+        [Inject] private IScoreView scoreView;
+
+        public void UpdateScore(int num)
+        {
+            scoreView.UpdateScore(num);
+        }
+    }
+}
+
